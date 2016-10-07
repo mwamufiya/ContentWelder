35095db5f9f027669e7c8f653aada05a38294544
mwamufiya <mwamufiya@gmail.com>
2016-10-07 15:21:39 -0400

    Remove untracked files

--- app/widget-configuration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var WidgetConfiguration = (function () {
-    function WidgetConfiguration() {
-    }
-    return WidgetConfiguration;
-}());
-exports.WidgetConfiguration = WidgetConfiguration;
-//# sourceMappingURL=widget-configuration.js.map
