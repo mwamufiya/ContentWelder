a49fd6e44d978da0564b1e627465ea8115abe24c
mwamufiya <mwamufiya@gmail.com>
2016-10-28 19:23:09 -0400

    removed forms for app routing, and into it's own module

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -14,11 +14,6 @@ const appRoutes: Routes = [
     {
         path:'image-chooser',
         component: ImageChooser
-    },
-    {
-        path:'forms',
-        component: FormWidget,
-
     }
 ];
 
