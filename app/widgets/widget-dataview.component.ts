1000f6b3f5525002af20b5422fab2340efd55ccd
mwamufiya <mwamufiya@gmail.com>
2016-10-30 12:04:23 -0400

    Widget now working with DataView-builder JSON

--- app/widgets/widget-dataview.component.ts
+++ app/widgets/widget-dataview.component.ts
@@ -1,9 +1,12 @@
 import { Component, ViewContainerRef, ComponentFactoryResolver, ChangeDetectorRef,
     ViewChild, forwardRef, HostListener, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
 import { Widget  } from './widget.component';
 import { DesignerGlobalsService } from '../services/designer-globals.service';
 import { Parent } from './parent';
 import { WidgetConfig } from './widget.interface';
+import { BuilderConfig } from './dataview-builder/dataview-builder.interface';
+import { DataViewBuilderService } from './dataview-builder/dataview-builder.services';
 
 @Component({
     moduleId: module.id,
@@ -21,25 +24,29 @@ import { WidgetConfig } from './widget.interface';
 
 export class DataviewWidget extends Widget implements OnDestroy{
     widgetType:string = 'dataviewwidget';
-    restConfig:RestConfig;
-    dispSettings:boolean;
+    dispBuilderConfig:boolean;
+    dataBuilerConfig: BuilderConfig;
+    loadingData:boolean;            //Indicator for when data is being requested over HTTP
+    dataList:JSON;           //Stores data returned from server
 
     constructor(
         private componentFactoryResolver:ComponentFactoryResolver,
         private viewContainer:ViewContainerRef,
+        private dataBuilderService: DataViewBuilderService,
         changeDetectorRef: ChangeDetectorRef,
         designerGlobals: DesignerGlobalsService){
 
         super(componentFactoryResolver, viewContainer, changeDetectorRef, designerGlobals);
 
-        //Debug only
-        //TODO remove this once integrated with Designer
-        this.isSelected = true;
-        this.restConfig = {};
-        this.restConfig.httpMethod = 'get';
-
     }
-    @HostListener('click', ['$event']) onclick(event):boolean{
+
+    /**
+     * function
+     * @param {Event} event
+     * @returns {boolean}
+     * @description Handles the click action
+     */
+    @HostListener('click', ['$event']) onclick(event: Event):boolean{
         return super.onclick(event);
     }
 
@@ -48,19 +55,45 @@ export class DataviewWidget extends Widget implements OnDestroy{
      * @description toggles the settings screen
      */
     toggleSettings():void{
-        this.dispSettings = (this.dispSettings==true)? null : true;
+        this.dispBuilderConfig = (this.dispBuilderConfig==true)? null : true;
     }
 
-    supportedRestMethods(): Array<string>{
-        return new Array('get','post','put','patch','delete','head','options');
-    }
-    setRequestMethod(method: string){
-        if( this.supportedRestMethods().indexOf(method.toLowerCase()))
-            this.restConfig.httpMethod = method;
+    /**
+     * @function
+     * @param {BuilderConfig} data
+     * @description sets the current
+     */
+    setBuilderConfig(config: BuilderConfig):void{
+        this.dataBuilerConfig = config;
     }
 
-    getData(){
-        console.log(this.restConfig);
+    /**
+     * @function
+     * @param {BuilderConfig} data
+     * @description
+     *
+     */
+    displayData(config:BuilderConfig):void{
+        this.setBuilderConfig(config);
+        this.fetchData();                                       //Make Call to get data
+    }
+
+    /**
+     * @function
+     * @description fetches data.
+     */
+    fetchData():void{
+        this.dispBuilderConfig = null;                          //hide the builder config as the user is done now
+        this.loadingData = true;                                //Allow the UI to display any 'loading' based on this
+        this.dataList = null;                                   //Clear out the existing Data list in preparation for new data
+        this.dataBuilderService.fetch(this.dataBuilerConfig)    //Fetch the data
+            .then( json => {
+                this.loadingData = false;
+                this.dataList = json;
+            })
+            .catch( response => {
+                //TODO handle error
+            });
     }
 
     /**
@@ -72,6 +105,7 @@ export class DataviewWidget extends Widget implements OnDestroy{
         let json = super.toJson();
 
         //Handle Object specific logic
+        json['dataBuilerConfig'] = this.dataBuilerConfig;
 
         return json;
     }
@@ -91,6 +125,11 @@ export class DataviewWidget extends Widget implements OnDestroy{
         super.parseWidgetConfig(this.widgetConfig);
 
         //now perform processes specific to this object
+        if(this.widgetConfig['dataBuilerConfig']) {
+            this.dataBuilerConfig = this.widgetConfig['dataBuilderConfig'];
+            this.fetchData();
+
+        }
 
     }
 
@@ -101,10 +140,4 @@ export class DataviewWidget extends Widget implements OnDestroy{
     ngOnDestroy():void{
         super.ngOnDestroy();
     }
-}
-
-export interface RestConfig{
-    httpMethod?: {};             //GET, PUT, POST, DELETE, OPTIONS...
-    host?: string;               //URL
-    path?: string;               //"url/path"
 }
